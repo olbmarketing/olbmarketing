c43065badf98de7363f4ef3b5e8770d96eb58a72
David Browning <dave@Davids-MacBook-Pro.local>
2016-09-20 20:13:33 -0400

    Moved the dashboard into a controller. Added authentication to the static_page controller and student controller. Added links to the student pages in the dashboard

--- OLOB_app/app/controllers/students_controller.rb
+++ OLOB_app/app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_filter :authorize
   before_action :set_student, only: [:show, :edit, :update, :destroy]
 
   # GET /students
--- OLOB_app/config/routes.rb
+++ OLOB_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/dashboard'
+
   resources :students
 	root to: 'sessions#new'
 
