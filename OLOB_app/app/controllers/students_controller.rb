c064dff981154164879fb6c70f369d025d35a667
David Browning <dave@Davids-MacBook-Pro.local>
2016-09-29 14:41:48 -0400

    Added student export function to dashboard

--- OLOB_app/app/controllers/students_controller.rb
+++ OLOB_app/app/controllers/students_controller.rb
@@ -62,6 +62,15 @@ class StudentsController < ApplicationController
     end
   end
 
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |product|
+        csv << product.attributes.values_at(*column_names)
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_student
