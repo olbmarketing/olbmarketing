12254af9db416ebd73b340bf854a7990cb3aac13
Justin Forsthoefel <biohazard_j@outlook.com>
2016-10-19 19:39:31 -0400

    Minor additions to layers.js

--- OLOB_app/app/assets/javascripts/layers.js
+++ OLOB_app/app/assets/javascripts/layers.js
@@ -1,15 +1,24 @@
-function Layer( name, markers ) {
+function Layer( name, markers, map ) {
   this.name = name;
   this.markers = markers;
+  var map = map || null;
   this.addMarker = function( marker ) {
+    marker.map = map;
     this.markers.push( marker );
   }
   this.setMap = function( map ) {
-
+    this.markers.forEach( function( marker, index, array ) {
+      marker.map = map;
+    });
   }
 }
 
 function LayerGroup( name, layers ) {
   this.name = name;
   this.layers = layers || [];
+  this.setMap = function( map ) {
+    this.layers.forEach( function( layer, index, array ) {
+      layer.setMap( map )
+    }
+  }
 }
