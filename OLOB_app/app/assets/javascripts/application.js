f8f92f7a49ed4f9489c7ba45b5da50127f96ce1d
mendiola6 <mendiola.6@osu.edu>
2016-10-19 20:06:26 -0500

    updated table sorting- must fresh the page for Jquery to work

--- OLOB_app/app/assets/javascripts/application.js
+++ OLOB_app/app/assets/javascripts/application.js
@@ -11,8 +11,10 @@
 // about supported directives.
 //
 //= require jquery
+//= require datatables
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require jquery.turbolinks
 //= require turbolinks
 //= require angular/angular
 //= require ng-table
