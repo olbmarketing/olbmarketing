6b05e73ef23cfd0ad7f47771e7ca1c1601726283
scrobbins1 <robbins.262@osu.edu>
2016-11-23 13:03:15 -0500

    Changed destroy to delete and edited add student class field names

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20161122020017) do
     t.string   "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "schools", force: :cascade do |t|
