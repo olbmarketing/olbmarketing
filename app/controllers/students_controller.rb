92cf68c42c38ea55cea1fbe181d956e81f78df53
Xu <weng.88@buckeyemail.osu.edu>
2017-12-10 23:53:29 -0500

    optomize import update performance

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -90,10 +90,11 @@ class StudentsController < ApplicationController
     # check for errors before insert data
     students_from_file.each_with_index do |s, index|
       # check if duplicate with other studnents from file 
-      students_from_file.each_with_index do |s2, index2|
-        if index != index2 && s.first_name == s2.first_name && s.last_name == s2.last_name && s.school_year == s2.school_year
+      ((index + 1)..(students_from_file.count - 1)).each do |i|
+        s2 = students_from_file[i]
+        if s.first_name == s2.first_name && s.last_name == s2.last_name && s.school_year == s2.school_year
           all_valid = false
-          error_msg = "Duplicate students in data source: student at row #{index + 2} and student at row #{index2 + 2} has the same first_name, last_name, school_year"
+          error_msg = "Duplicate students in data source: student at row #{index + 2} and student at row #{i + 2} has the same first_name, last_name, school_year"
           @upload_errors << error_msg
           break
         end 
