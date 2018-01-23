0e6f712d66bf33ee01de81edeb3ea4472795db8c
Xu <weng.88@buckeyemail.osu.edu>
2018-01-22 22:12:47 -0500

    minor testing view update

--- app/models/star_math_test.rb
+++ app/models/star_math_test.rb
@@ -1,5 +1,5 @@
 class StarMathTest < ApplicationRecord
   belongs_to :student
-  validates_inclusion_of :scaled_score, :in => 200..450, :message => "must be within 200 to 450"
+  validates_inclusion_of :scaled_score, :in => 0..1400, :message => "must be within 0 to 1400"
   validates :test_date, presence: {message: "can't be blank or is incorrect format"}
 end
